8a144f5d16e817e987692a47c04b1036467f33e1
tobias-it-minds <tobias.frederiksen@it-minds.dk>
2026-09-09 12:51:52 +0200

    added db volume, healthcheck and api env

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,12 +1,24 @@
+using Npgsql;
+
+string host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+string port = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+string password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+string connectionString = $"Host={host};Port={port};Username=postgres;Password={password};Database=kanban-database";
+await using var connection = new NpgsqlConnection(connectionString);
+await connection.OpenAsync();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// builder.Services.AddAuthentication().AddGoogleOpenIdConnect(googleOptions =>
+// {
+//     googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+//     googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+// });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +26,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return "<h1> test </h1>";
 })
 .WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
