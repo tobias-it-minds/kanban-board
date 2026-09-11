31872f9574624e28c5bda0885b77b968e155bc11
tobias-it-minds <tobias.frederiksen@it-minds.dk>
2026-09-11 10:07:34 +0200

    removed config from git

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -8,16 +8,7 @@ import { initializeApp } from 'firebase/app';
 import { initializeUI } from '@firebase-oss/ui-core';
 import { FirebaseUIProvider } from '@firebase-oss/ui-react';
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB_UefwmC1_AGrJC1sVtOURTwIcRVeHHEc",
-  authDomain: "kanban-it-minds-97608.firebaseapp.com",
-  projectId: "kanban-it-minds-97608",
-  storageBucket: "kanban-it-minds-97608.firebasestorage.app",
-  messagingSenderId: "197668053186",
-  appId: "1:197668053186:web:400f525705d05ce0725b5b",
-  measurementId: "G-Q652L65K3C"
-}
+import firebaseConfig from '../../firebaseconfig'
 
 const THEME_INIT_SCRIPT = `(function(){try{var stored=window.localStorage.getItem('theme');var mode=(stored==='light'||stored==='dark'||stored==='auto')?stored:'auto';var prefersDark=window.matchMedia('(prefers-color-scheme: dark)').matches;var resolved=mode==='auto'?(prefersDark?'dark':'light'):mode;var root=document.documentElement;root.classList.remove('light','dark');root.classList.add(resolved);if(mode==='auto'){root.removeAttribute('data-theme')}else{root.setAttribute('data-theme',mode)}root.style.colorScheme=resolved;}catch(e){}})();`
 
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,33 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { getAuth } from "firebase/auth";
+
+
 export const Route = createFileRoute('/')({ component: App })
+
+async function createProject(project: string) {
+  const user = getAuth().currentUser;
+  if (user == null) return;
+  const idToken = await user.getIdToken(true);
+
+
+
+  var response = await fetch("localhost:5001/projects", {
+    method: "POST",
+    body: JSON.stringify({ Name: project })
+  });
+}
+
+
+
 
 function App() {
   return (
     <main className="page-wrap px-4 pb-8 pt-14">
+      <button onClick={() => {
+        createProject("Test Project")
+      }
+      }>Button</button>
     </main>
   )
 }
